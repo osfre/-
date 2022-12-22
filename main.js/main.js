9f6a83f46d9354d8660d55209cac15856832e090
osfre <freedosama34@gmail.com>
2022-12-22 13:09:46 +0200

    مسابقة

---
+++ main.js/main.js
@@ -0,0 +1,64 @@
+ScrollReveal().reveal(".box1", {
+    origin: "left",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".box2", {
+    origin: "bottom",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".box3", {
+    origin: "left",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".photo1", {
+    origin: "left",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".photo2", {
+    origin: "bottom",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".photo3", {
+    origin: "left",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".btn", {
+    origin: "bottom",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
+ScrollReveal().reveal(".card", {
+    origin: "bottom",
+    distance: "250px",
+    opacity: 0,
+    duration: 2000,
+    ease: "ease-in-out",
+    reset: true,
+  });
